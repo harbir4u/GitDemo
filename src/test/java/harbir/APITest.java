3e1ee42a6ac39a9f282aa2ec364e78c58798accd
harbir4u <harbir4u@gmail.com>
2019-07-19 12:21:23 -0400

    first commit

---
+++ src/test/java/harbir/APITest.java
@@ -0,0 +1,17 @@
+package harbir;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI() {
+		System.out.println("ElementsUI");
+	}
+
+}
---
+++ src/test/java/harbir/SeleniumTest.java
@@ -0,0 +1,15 @@
+package harbir;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void AndroidApp() {
+		System.out.println("AndroidApp");
+	}
+	
+	@Test
+	public void IOSApp() {
+		System.out.println("IOSApp");
+	}
+}
